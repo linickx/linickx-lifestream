f1740253d68023b2d33b412e5e4814603bca65cf
linickx <linickx@gmail.com>
2009-11-09 15:05:55 +0000

    Upload Config.sample.php to Trunk
    git-svn-id: http://plugins.svn.wordpress.org/linickx-lifestream@171727 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ trunk/config.sample.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+
+	This File is used to configure the plugin for Cron Updates,
+	You don't need to change anything if you're using WordPress Updates.
+
+	$WPDIR is the veriable of where WordPress is installed.
+	Please change as necessary, examples.
+
+	$WPDIR = "/blog"; // www.domain.com/blog
+	$WPDIR = ""; // www.domain.com
+	$WPDIR = "/wordpres"; // www.domain.com/wordpress
+
+	When Finished Rename this file to config.php 
+	~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+*/
+
+	$WPDIR = "/wordpress"; // by Default we thing you're website is www.domain.com/wordpress
+
+
+
+?>
